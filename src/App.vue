f356cac4dc7e1434cc98307508db667ebc84f94c
Frnak <francisco@glide.com>
2020-09-26 15:27:43 -0300

    Initial layout

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="column">
+    <navbar></navbar>
+    <h1>Ignacio Francisco Janin Iglesias</h1>
+    <h2>Frotnend developer</h2>
+    <span>I'm a graduate software engineer from UTN. I'm currently working at Wolox as Front-End Engineering Manager using technologies such as React, Angular and Vue. I also love doing research on new technologies and putting them into practice in personal projects.</span>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components: { 
+    Navbar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./scss/application";
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="row space-between">
+    <a href="/">Frank</a>
+    <div class="row">
+      <a
+        v-for="socialLink in socialLinks"
+        :key="socialLink.link"
+        :href="socialLink.link"
+        target="_blank"
+      >
+        <img :src="socialLink.img" />
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      socialLinks: [
+        {
+          img: "image",
+          link: "",
+        },
+        {
+          img: "image",
+          link: "",
+        },
+        {
+          img: "image",
+          link: "",
+        },
+        {
+          img: "image",
+          link: "",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style lang="scss" scoped>
+</style>
