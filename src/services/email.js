78476cb1586b1e216ee31e19a74eaeb2c132f044
FrankIglesias <ifrancisco.iglesias@gmail.com>
2021-05-05 14:04:14 -0300

    Changed endpoints

--- src/services/email.js
+++ src/services/email.js
@@ -1,5 +1,5 @@
 import api from '../config/api';
 
 export function sendEmail(values) {
-  return api.post('https://go-mailer.franciscojiglesias.vercel.app/api/', values);
+  return api.post('https://go-mailer.franciscojiglesias.vercel.app/api/email', values);
 }
--- src/services/github.js
+++ src/services/github.js
@@ -2,5 +2,5 @@ import api from '../config/api';
 
 
 export function getRepos() {
-  return api.get('https://api.github.com/users/Frankiglesias/repos');
+  return api.get('https://api.github.com/users/Frankiglesias/repos',{}, {headers: {accept: 'application/vnd.github.mercy-preview+json'}});
 }
